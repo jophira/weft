7c81704e948c0139a53fd2abf23457f252465f01
philip-codes <philip.webe@gmail.com>
2026-06-04 14:18:19 +0200

    feat: add goreleaser

--- cmd/version.go
+++ cmd/version.go
@@ -6,13 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.1.0-dev"
+var (
+	Version = "0.1.0-dev"
+	Commit  = "none"
+	Date    = "unknown"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("weft %s\n", Version)
+		fmt.Printf("weft %s (commit %s, built %s)\n", Version, Commit, Date)
 	},
 }
 
